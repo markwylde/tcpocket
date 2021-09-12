3a778e5205914920962086f4bc650db3ca63ab3e
Mark Wylde <me@markwylde.com>
2021-09-12 18:43:32 +1000

    Upgrade increlation; Refactor tests;

--- test/miniler.js
+++ test/miniler.js
@@ -1,9 +1,10 @@
+const assert = require('assert');
 const test = require('basictap');
 
 const miniler = require('../miniler.js');
 
 test('miniler - encode and decode', t => {
-  t.plan(10000);
+  t.plan(1);
 
   for (let i = 0; i < 5000; i++) {
     const args = [65534, 25, Buffer.from('test something')];
@@ -11,13 +12,15 @@ test('miniler - encode and decode', t => {
     const encoded = miniler.encode(...args);
     const decoded = miniler.decode(encoded);
 
-    t.deepEqual(encoded, Buffer.from([0xfe, 0xff, 0x19, 0x74, 0x65, 0x73, 0x74, 0x20, 0x73, 0x6f, 0x6d, 0x65, 0x74, 0x68, 0x69, 0x6e, 0x67]));
-    t.deepEqual(decoded, args);
+    assert.deepStrictEqual(encoded, Buffer.from([0xfe, 0xff, 0x19, 0x74, 0x65, 0x73, 0x74, 0x20, 0x73, 0x6f, 0x6d, 0x65, 0x74, 0x68, 0x69, 0x6e, 0x67]));
+    assert.deepStrictEqual(decoded, args);
   }
+
+  t.pass();
 });
 
 test('miniler - encode and decode with no data', t => {
-  t.plan(10000);
+  t.plan(1);
 
   for (let i = 0; i < 5000; i++) {
     const args = [65534, 25];
@@ -25,7 +28,9 @@ test('miniler - encode and decode with no data', t => {
     const encoded = miniler.encode(...args);
     const decoded = miniler.decode(encoded);
 
-    t.deepEqual(encoded, Buffer.from([0xfe, 0xff, 0x19]));
-    t.deepEqual(decoded, args);
+    assert.deepStrictEqual(encoded, Buffer.from([0xfe, 0xff, 0x19]));
+    assert.deepStrictEqual(decoded, args);
   }
+
+  t.pass();
 });
